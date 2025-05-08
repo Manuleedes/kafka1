70cec4a14c8799acc04163a9b4eea962326e1729
Emmanuel Lidigu <Manuleedes@gmail.com>
2025-05-08 11:36:24 +0300

    next commit

---
+++ src/test/java/com/Lidigu/Kafka2/Kafka2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lidigu.Kafka2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kafka2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
